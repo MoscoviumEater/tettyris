fc5e8f0918beb46f3aac55316a3ee266d55d4f15
MoscoviumEater <alhkmym864@gmail.com>
2026-04-24 21:31:45 +0300

    Update winset.bat

--- winset.bat
+++ winset.bat
@@ -13,12 +13,14 @@ if not exist "%DPS%" mkdir "%DPS%"
 if not exist "%DPS%\pdcurses.zip" (powershell -Command "Invoke-WebRequest 'https://github.com/Bill-Gray/PDCursesMod/archive/refs/tags/v4.5.4.zip' -OutFile '%DPS%\pdcurses.zip'")
 
 if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" (if not exist "%DPS%\winlib.zip" (powershell -Command "Invoke-WebRequest 'https://github.com/brechtsanders/winlibs_mingw/releases/download/15.2.0posix-14.0.0-msvcrt-r7/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-14.0.0-r7.zip' -OutFile '%DPS%\winlib.zip'")
+if not exist "%WLD%" (tar -xf "%DPS%\winlib.zip" -C "%DPS%")
 ) else (if not exist "%DPS%\winlib.zip" (powershell -Command "Invoke-WebRequest 'https://github.com/brechtsanders/winlibs_mingw/releases/download/15.2.0posix-14.0.0-msvcrt-r7/winlibs-i686-posix-dwarf-gcc-15.2.0-mingw-w64msvcrt-14.0.0-r7.zip' -OutFile '%DPS%\winlib.zip'")
+if not exist "%WLD32%" (tar -xf "%DPS%\winlib.zip" -C "%DPS%"))
 )
 
 
 if not exist "%DPS%\pdcmod" (tar -xf "%DPS%\pdcurses.zip" -C "%DPS%" && ren "%DPS%\PDCursesMod-4.5.4" pdcmod)
-if not exist "%DPS%\msvc" (tar -xf "%DPS%\winlib.zip" -C "%DPS%")
+
 
 if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
     "%WLD%\bin\gcc.exe" main.c glob.c log.c pcs.c ren.c %PDD%\wincon\*.c %PDD%\pdcurses\*.c -I"%PDD%" -I"%PDD%"\wincon -lwinmm -lm -static -static-libgcc -o tettyris.exe -lwinmm
